52c8fc3dd54602b1950bbfdea967150b3534a96b
Christina Brady <=>
2021-09-13 15:39:07 -0400

    refactored poor mans bar chart to match code in the book

--- letter_bar_chart.py
+++ letter_bar_chart.py
@@ -1,26 +1,22 @@
 import re
+from collections import defaultdict
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 print("Welcome to the poor man's bar chart")
 
 user_input = input("Please give me a sentence.\n")
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
 def letter_count(sentence):
     ''' This function takes a string, counts the number of times
         each letter appears, and returns a stem and leaf plot
         of the letter appearances.
     '''
-
-    no_punct = re.sub(r'[^\w\s]+', '', sentence.lower())
-    no_new_line = re.sub('\\n', '', no_punct)
-    no_space = re.sub(r'\s+', '', no_new_line)
-    chars = list(no_space)
-    chars.sort()
-    keylist = set(chars)
-    d = {}
-    for key in keylist:
-        d[key] = [char for char in chars if char == key]
-    pp.pprint(d)
+    mapped = defaultdict(list)
+    for char in sentence:
+        if char in ALPHABET:
+            mappedsd[char].append(char)
+    pp.ppint(mapped)
 
 letter_count(user_input)
